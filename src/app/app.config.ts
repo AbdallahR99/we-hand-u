f9eaaa945cd8713fc4606ac2cce14855f9f90e5d
Abdallah Elrashedy <abodyrody55@gmail.com>
2024-09-20 21:12:25 +0400

    fixed sass and added material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,27 @@ import {
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
+import { LocalStorageKeys } from '@core/constants/local_storage';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+
+export function tokenGetter() {
+  return localStorage.getItem(LocalStorageKeys.token);
+}
+// export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(),
+    provideAnimations(),
     provideHttpClient(
       withFetch(),
       withInterceptors([requestContextInterceptor])
     ),
     provideClientHydration(),
+    provideContent(withMarkdownRenderer()),
   ],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,48 @@
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
 import angular from '@analogjs/vite-plugin-angular';
+import { resolve } from 'path';
+// import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       scss: {
+  //         includePaths: [
+  //           resolve(__dirname, 'src/assets/styles'),
+  //           '@assets/styles',
+  //         ],
+  //       },
+  //     },
+  //   },
+  // },
+  // css: {
+  //   preprocessorOptions: {
+  //     includePaths: [resolve(__dirname, 'src/assets/styles'), '@assets/styles'],
+  //     css: {
+  //       includePaths: [
+  //         resolve(__dirname, 'src/assets/styles'),
+  //         '@assets/styles',
+  //       ],
+  //     },
+  //     scss: {
+  //       api: 'modern-compiler', // or "modern",
+  //       includePaths: [
+  //         resolve(__dirname, 'src/assets/styles'),
+  //         '@assets/styles',
+  //       ],
+  //     },
+  //   },
+  // },
+
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler', // or "modern"
+        // api: 'modern-compiler', // or "modern",
+        includePaths: ['src/assets/styles'],
       },
     },
   },
@@ -18,9 +53,33 @@ export default defineConfig(({ mode }) => ({
   },
   resolve: {
     mainFields: ['module'],
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@core': resolve(__dirname, 'src/app/core'),
+      '@pages': resolve(__dirname, 'src/app/pages'),
+      '@environments': resolve(__dirname, 'src/app/environments'),
+      '@app': resolve(__dirname, 'src/app'),
+      '@server': resolve(__dirname, 'src/server'),
+    },
   },
+
   plugins: [
-    analog(),
+    viteTsConfigPaths(),
+
+    analog({
+      nitro: {
+        preset: 'node-server',
+
+        // rollupConfig: {
+        //   plugins: [
+        //     typescriptPaths({
+        //       tsConfigPath: 'tsconfig.json',
+        //       preserveExtensions: true,
+        //     }) as any,
+        //   ],
+        // },
+      },
+    }),
     angular({
       inlineStylesExtension: 'scss',
     }),
